e1ac799c8dd5ddf0da7799a2dda078d40bc66762
mancho-ged <mancho-ged@users.noreply.github.com>
2024-07-26 23:51:59 +0400

    06-Lets explore the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,55 @@
 import RestaurantCard from './RestaurantCard';
-import resList from '../utils/mockData';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Shimmer from './Shimmer';
 
 const Body = () => {
-  const [listOfRestorants, setListOfRestorants] = useState(resList.categories);
+  const [listOfRestorants, setListOfRestorants] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-  return (
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.themealdb.com/api/json/v1/1/categories.php'
+    );
+    const json = await data.json();
+    console.log(json);
+    setListOfRestorants(json?.categories);
+    setFilteredRestaurants(json?.categories);
+  };
+
+  // Conditional Rendering
+  return listOfRestorants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-input"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const newListOfRestorants = listOfRestorants.filter((res) =>
+                res.strCategory.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurants(newListOfRestorants);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -21,7 +63,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestorants.map((res) => (
+        {filteredRestaurants.map((res) => (
           <RestaurantCard resData={res} key={res.idCategory} />
         ))}
       </div>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,12 @@
+const Shimmer = () => (
+  <div className="shimmer-container">
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+  </div>
+);
+
+export default Shimmer;
